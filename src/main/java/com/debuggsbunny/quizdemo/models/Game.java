1549dd356fadb8af392ef19ea56bf8ff14f2562c
Lucky <luckylalgupta@gmail.com>
2021-01-09 11:50:13 +0530

    my seventh commit

--- src/main/java/com/debuggsbunny/quizdemo/models/Game.java
+++ src/main/java/com/debuggsbunny/quizdemo/models/Game.java
@@ -2,14 +2,31 @@ package com.debuggsbunny.quizdemo.models;
 
 import com.sun.javafx.geom.transform.Identity;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class Game {
     @javax.persistence.Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @ManyToOne
+    private Quiz quiz;
+
+    @Column
+    private LocalDateTime startedAt;
+
+    @Column
+    private LocalDateTime endedAt;
+
+    @ManyToMany
+    private User player;
+
+    @OneToOne
+    private Result result;
+
+    @OneToMany
+    private List<Response> responses;
 
 }
