80b51eb70cb2fcd512fae0e1eb8adc91002c79e1
Lucky <luckylalgupta@gmail.com>
2021-01-09 10:32:06 +0530

    my sixth commit

--- src/main/java/com/debuggsbunny/quizdemo/models/Result.java
+++ src/main/java/com/debuggsbunny/quizdemo/models/Result.java
@@ -1,4 +1,16 @@
 package com.debuggsbunny.quizdemo.models;
 
+import javax.persistence.*;
+
 public class Result {
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+
+    @OneToOne
+    @Column
+    private Game game;
+
+    @Column
+    private Integer score;
 }
